675b097576fc79dfc314498c4323566c5edac304
Lluís Espert Cuquerella <espertcuquerellalluis@gmail.com>
2024-10-14 12:56:20 +0200

    Terminado

---
+++ postal.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/estilos.css" />
+</head>
+<body>
+    <form class="grosor anchoFormulario botonCentrado">
+        <div class="row">
+            <div class="columna">
+
+                <h2>My Postcard</h2>
+
+                <p>Look how awesome! This is a postcard that i created using HTML5 and CSS3 during my 4Geeks Academy Course</p>
+
+                <br>
+
+                <p>This is so cool, can't wait to start doing more stuff!!</p>
+
+            </div>
+            <div class="columna">
+
+                <img src="img/4geeks.png" width="60" height="60" class="imagen">
+
+                <br>
+
+                <br>
+
+                <br>
+
+                <br>
+
+                <br>
+
+                <input type="text" id="nombre" name="nombre" value="Introduce el nombre">
+                
+                <br>
+
+                <br>
+
+                <br>
+
+                <input type="text" id="email" nombre="email" value="Introduce el email">
+
+                <br>
+
+                <br>
+
+                <br>
+
+                <input type="text" id="comentario" nombre="comentario" value="Introduce el comentario">
+
+            </div>
+        </div>
+
+        <br>
+
+        <br>
+
+
+        <input type="submit" value="Send my postcard" class="botonCentrado">
+
+        <br>
+
+    </form>
+</body>
+</html>
